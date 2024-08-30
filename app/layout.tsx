434bc25910672fa8d5e5829691ed8835bf1e2a70
Sean Rezaie <seanrezaie@berkeley.edu>
2024-08-30 13:05:40 -0700

    icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
+      <body>{children}</body>
     </html>
   );
 }
