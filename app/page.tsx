820b430610b213538225aa52538fae2ce4170fae
Sean Rezaie <seanrezaie@berkeley.edu>
2024-08-31 16:46:20 -0700

    fin

--- app/page.tsx
+++ app/page.tsx
@@ -80,7 +80,7 @@ export default function Portfolio() {
       <Image
         alt="Sean Rezaie"
         className="rounded-full object-cover border-4 border-white shadow-lg"
-        src="/IMG_6705.jpg"
+        src="/IMG_1836 5.jpg"
         width={160}
         height={160}
       />
@@ -181,6 +181,42 @@ export default function Portfolio() {
               description: [
                 "Developed and implemented a k-Nearest Neighbors (k-NN) classifier to analyze the correlation between COVID-19 impact, demographic data, and political affiliation in U.S counties during the 2020 Presidential Election",
                 "Achieved a predicted accuracy of 92.6% by utilizing machine learning techniques to process and analyze data from sources such as New York Times, Politico, The Census Bureau, and the EPA"
+              ]
+            },
+            {
+              title: "Build Your Own World",
+              date: "Jul 2023",
+              description: [
+                "Implemented a procedurally generated world engine in a team-based project, focusing on inherent software-development cycles",
+                "Leveraged iterative design principles and selective use of data structures and algorithms to create a tile-based 2D video game"
+              ]
+            },
+            {
+              title: "CPU",
+              date: "Mar 2024",
+              description: [
+                "Simulated a functional CPU that supports the RISC-V instruction set, using Logisim",
+              ]
+            },
+            {
+              title: "2048",
+              date: "Sep 2023",
+              description: [
+                "Recreated the sliding-tile game 2048, using Java and other Object Oriented Programming essentials."
+              ]
+            },
+            {
+              title: "Ants vs. SomeBees", 
+              date: "Jul 2023",
+              description: [
+                "Recreated Plants vs. Zombies in Python using OOP principles with a special emphasis on inheritance to design various game characters and scenarios."
+              ]
+            },
+            {
+              title: "Snake",
+              date: "Jan 2024",
+              description: [
+                "Developed and implemented the logic of the popular game Snake, using the C programming language with manual memory allocation"
               ]
             }
           ].map((project, index) => (
